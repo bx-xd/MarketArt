a47306f2bc547046628424c76ea8f15fa6ff545d
Antoine Brique <antoine.brique@gmail.com>
2021-12-13 11:33:46 +0100

    add validations to Artwork, presence of title, description and price greater than 0

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,4 +1,8 @@
 class Artwork < ApplicationRecord
   belongs_to :user
   has_many_attached :photo
+
+  validates :title, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
+  validates :price, numericality: { greater_than: 0 }
 end
