aa7615bd0e18dca32c014ea2fbc8a10260802d2b
Antoine Brique <antoine.brique@gmail.com>
2021-12-13 18:20:47 +0100

    add exposition and about method to PagesController, add routes corresponding

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :about, :exposition ]
 
   def home
+    @artworks = Artwork.all
+  end
+
+  def about
+  end
+
+  def exposition
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'about', to: 'pages#about', as: 'about'
+  get 'exposition', to: 'pages#exposition', as: 'exposition'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :artworks, only: [:index, :show]
 end
