1f61083d0f35e15e25c588209a690ec7c1b518b0
Antoine Brique <antoine.brique@gmail.com>
2021-12-13 10:51:00 +0100

    add firstName and lastName to User, add validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
+
   has_many :orders
   has_many :artworks
 end
